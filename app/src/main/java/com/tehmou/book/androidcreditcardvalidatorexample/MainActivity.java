5c41799c501a14025804d5e60d2436e2fbabe7b4
tehmou <timo.tuominen@gmail.com>
2017-03-08 19:42:52 +0000

    Add calculation of validity

--- app/src/main/java/com/tehmou/book/androidcreditcardvalidatorexample/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidcreditcardvalidatorexample/MainActivity.java
@@ -1,13 +1,101 @@
 package com.tehmou.book.androidcreditcardvalidatorexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.tehmou.book.androidcreditcardvalidatorexample.utils.CardType;
+import com.tehmou.book.androidcreditcardvalidatorexample.utils.ValidationUtils;
+
+import java.util.Arrays;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener((view) -> {
+            Log.d(TAG, "Submit");
+            findViewById(R.id.container).requestFocus();
+        });
+
+        final EditText creditCardNumberView = (EditText) findViewById(R.id.credit_card_number);
+        final EditText creditCardCvcView = (EditText) findViewById(R.id.credit_card_cvc);
+        final TextView creditCardType = (TextView) findViewById(R.id.credit_card_type);
+        final TextView errorText = (TextView) findViewById(R.id.error_text);
+
+        final Observable<String> creditCardNumber =
+                RxTextView.textChanges(creditCardNumberView)
+                        .map(CharSequence::toString);
+
+        final Observable<String> creditCardCvc =
+                RxTextView.textChanges(creditCardCvcView)
+                        .map(CharSequence::toString);
+
+        final Observable<CardType> cardType =
+                creditCardNumber
+                        .map(CardType::fromNumber);
+
+        final Observable<Boolean> isKnownCardType =
+                cardType
+                        .map(cardTypeValue -> cardTypeValue != CardType.UNKNOWN);
+
+        final Observable<Boolean> isValidCheckSum =
+                creditCardNumber
+                        .map(ValidationUtils::checkCardChecksum);
+
+        final Observable<Boolean> isValidNumber =
+                Observable.combineLatest(
+                        isKnownCardType,
+                        isValidCheckSum,
+                        (isValidType, isChecksumCorrect) -> isValidType && isChecksumCorrect);
+
+        final Observable<Boolean> isValidCvc =
+                Observable.combineLatest(
+                        cardType,
+                        creditCardCvc,
+                        ValidationUtils::isValidCvc);
+
+        cardType
+                .map(Enum::toString)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(creditCardType::setText);
+
+        Observable.combineLatest(
+                isValidNumber,
+                isValidCvc,
+                (isValidNumberValue, isValidCvcValue) -> isValidNumberValue && isValidCvcValue)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(submitButton::setEnabled);
+
+        Observable.combineLatest(
+                Arrays.asList(
+                        isKnownCardType.map(value -> value ? "" : "Unknown card type"),
+                        isValidCheckSum.map(value -> value ? "" : "Invalid checksum"),
+                        isValidCvc.map(value -> value ? "" : "Invalid CVC code")),
+                (errorStrings) -> {
+                    StringBuilder builder = new StringBuilder();
+                    for (Object errorString : errorStrings) {
+                        if (!"".equals(errorString)) {
+                            builder.append(errorString);
+                            builder.append("\n");
+                        }
+                    }
+                    return builder.toString();
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(errorText::setText);
+
     }
 }
---
+++ app/src/main/java/com/tehmou/book/androidcreditcardvalidatorexample/utils/CardType.java
@@ -0,0 +1,41 @@
+package com.tehmou.book.androidcreditcardvalidatorexample.utils;
+
+import java.util.regex.Pattern;
+
+public enum CardType {
+    UNKNOWN(-1),
+    VISA(3),
+    MASTER_CARD(3),
+    AMERICA_EXPRESS(4);
+
+    private final int cvcLength;
+
+    CardType(int cvcLength) {
+        this.cvcLength = cvcLength;
+    }
+
+    public int getCvcLength() {
+        return cvcLength;
+    }
+
+    public static CardType fromNumber(String number) {
+        if (regVisa
+                .matcher(number).matches()) {
+            return VISA;
+        } else if (regMasterCard
+                .matcher(number).matches()) {
+            return MASTER_CARD;
+        } else if (regAmericanExpress
+                .matcher(number).matches()) {
+            return AMERICA_EXPRESS;
+        }
+        return UNKNOWN;
+    }
+
+    private static Pattern regVisa =
+            Pattern.compile("^4[0-9]{12}(?:[0-9]{3})?$");
+    private static Pattern regMasterCard =
+            Pattern.compile("^5[1-5][0-9]{14}$");
+    private static Pattern regAmericanExpress =
+            Pattern.compile("^3[47][0-9]{13}$");
+}
